3317f03d8342fe752ca89189459fa1ca7fec3f4c
vaish shivkumar <vaish.1231993.vaishali@gmail.com>
2025-07-19 11:34:39 +0100

    ganchane

--- contracts/hardhat.config.cjs
+++ contracts/hardhat.config.cjs
@@ -17,5 +17,9 @@ module.exports = {
       url: process.env.SEPOLIA_RPC,
       accounts: [process.env.PRIVATE_KEY],
     },
+    ganache: {
+      url: "http://127.0.0.1:8545",
+      accounts: [process.env.GANACHE_KEY]
+    }
   },
 };
